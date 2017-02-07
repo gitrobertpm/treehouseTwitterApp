4b797f36f5aa6e2d7839b8a50f87560bf8c1ba39
Robert Manolis <robertpm@comcast.net>
2017-02-07 01:32:44 -0800

    completed project

--- app.js
+++ app.js
@@ -1,21 +1,94 @@
-'use strict';
+/*************************************************************
+ * My Treehouse Projects
+ *
+ * FSJS proj #7 
+ * 
+ * Twitter Interface
+ *
+ * Robert Manolis, Milwaukie OR, February - 2017  :)
+ *************************************************************/
+(function() {
+	"use strict";
 
-var express = require('express');
+	var express = require("express");
+	var Twit = require("twit");
+	var config = require("./config.js");
+	
+	var T = new Twit(config);
 
-var app = express();
+	var app = express();
 
-app.use('/static', express.static(__dirname + '/public'));
+	app.use('/static', express.static(__dirname + '/public'));
 
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/templates');
+	app.set('view engine', 'pug');
+	app.set('views', __dirname + '/templates');
 
-app.get('/', function(req, res) {
-	var path = req.path;
-	//res.locals.path = path;
-	res.render('index');
-	//res.send("test");
-});
+	var friends = {};
+	var timeline = {};
+	var direct_messages = {};
+	var sent_messages = {};
+	
+	// HELPER FUNCTION FOR GETTING TWITTER DATA AND PASSING IT TO A CALLBACK FOR LATER USE
+	function getData(callback) {
+		
+		// FOLLOWING
+		T.get('friends/list', function(err, data, response) {
+			if(err) {
+				throw error;
+			} else {
+				friends = data.users;
+			}
+		});
+		
+		// TWEETS
+		T.get('statuses/user_timeline', function(err, data, response) {
+			if(err) {
+				throw error;
+			} else {
+				timeline = data;
+			}
+		});
+		
+		// DIRECT MESAGES I'VE SENT
+		T.get('direct_messages', function(err, data, response) {
+			if(err) {
+				throw error;
+			} else {
+				direct_messages = data;
+			}
+		});
+		
+		// DIRECT MESAGES I'VE RECEIVED
+		T.get('direct_messages/sent', function(err, data, response) {
+			if(err) {
+				throw error;
+			} else {
+				sent_messages = data;
+			} 
+		});
+		
+		callback();
+	}
 
-app.listen(3000, function() {
-	console.log("The frontend server is running on port 3000!");
-});
+	// RENDER DATA AND HANDLE ROUTES
+	getData(function() {
+		app.get('/', function(req, res) {
+			var path = req.path;	
+			res.render('index', {friends: friends, timeline: timeline, direct_messages: direct_messages, sent_messages: sent_messages});
+		});
+		
+		// IF BROWSER IS POINTED AT ANYTHING BUT THE HOME ROUTE 
+		// REDIRECT TO NICE ERROR PAGE
+		app.get('/:param?', function(req, res) {
+			var param = req.params.param;	
+			
+			if (param) {
+				res.render("error", {timeline: timeline, isWrongPath: true});
+			}
+		});
+	});
+
+	app.listen(3000, function() {
+		console.log("The frontend server is running on port 3000!");
+	});
+})();
